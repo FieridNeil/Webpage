e01900a48933670ba10d1517e9a78584c275fca4
Fierid Neil <14002607+FieridNeil@users.noreply.github.com>
2018-02-01 23:25:24 -0800

    update

--- Calendar/js/calendar.js
+++ Calendar/js/calendar.js
@@ -1,17 +1,17 @@
-class Calendar{
-  constructor(month, year){
+class Calendar {
+  constructor(month, year) {
     this.month = month;
     this.year = year;
   }
 
   // Display the days from mon -> sun
-  ShowDays(){
+  ShowDays() {
     var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     var container = document.getElementById('calendar_header');
     var ul = document.createElement("ul");
     ul.id = "calendar";
     container.appendChild(ul);
-    for(var i = 0; i < 7; i++){
+    for (var i = 0; i < 7; i++) {
       var li = document.createElement("li");
       li.className = "day";
       var text = document.createTextNode(days[i]);
@@ -21,23 +21,24 @@ class Calendar{
   }
 
   // Calculate how many days in a given month
-  daysInMonth () {
+  daysInMonth() {
     return new Date(this.year, this.month, 0).getDate();
   }
 
   // Display every date in a given month
-  ShowDates(){
+  ShowDates() {
     var container = document.getElementById('calendar_body');
     var ul = document.createElement("ul");
     ul.id = "calendar";
     container.appendChild(ul);
     var day = 1;
-    for(var i = 0; day <= this.daysInMonth(); i++){
+    // Print out all the dates in a given month
+    for (var i = 0; day <= this.daysInMonth(); i++) {
       var li = document.createElement("li");
-      if(new Date(this.year, this.month - 1, 1).getDay() - i > 0){
+      if (new Date(this.year, this.month - 1, 1).getDay() - i > 0) {
         li.className = "not_date";
         var node = document.createTextNode("");
-      }else{
+      } else {
         li.className = "date";
         var node = document.createTextNode(day);
         day++;
@@ -45,11 +46,26 @@ class Calendar{
       li.appendChild(node);
       ul.appendChild(li);
     }
+
+    // Fill the remaining empty spots on the last row if any
+    var index = 6;
+    while (index - new Date(this.year, this.month, 0).getDay() > 0) {
+      var li = document.createElement("li");
+      li.className = "not_date";
+      var node = document.createTextNode("");
+      li.appendChild(node);
+      ul.appendChild(li);
+      index--;
+    }
   }
 
-  RemoveDates(){
+  RemoveDates() {
     var container = document.getElementById('calendar_body');
     container.removeChild(container.childNodes[1]);
   }
 
+
+  CreateEvent(){
+
+  }
 }
--- Calendar/js/main.js
+++ Calendar/js/main.js
@@ -1,42 +1,66 @@
 var month = new Date().getMonth() + 1; // Month used based 0 so month 0 = jan
-
 var year = new Date().getFullYear();
 
 
-var c =  new Calendar(month, year);
-
+var c = new Calendar(month, year);
 c.ShowDays();
 c.ShowDates();
 
-
-var next_month = document.getElementById('next_month');
-next_month.addEventListener('click', GetNextMonth);
-
-function GetNextMonth(){
+// Display next month
+document.getElementById('next_month').addEventListener('click', function() {
   c.RemoveDates();
   month++;
-  c =  new Calendar(month, year);
+  c = new Calendar(month, year);
   c.ShowDates();
-}
+});
 
-
-var prev_month = document.getElementById('prev_month');
-prev_month.addEventListener('click', GetPrevMonth);
-
-function GetPrevMonth(){
+// Display previous month
+document.getElementById('prev_month').addEventListener('click', function() {
   c.RemoveDates();
   month--;
-  c =  new Calendar(month, year);
+  c = new Calendar(month, year);
   c.ShowDates();
-}
+});
+
 
 
 
 // Add click event delegate to each date in the calendar
 //childNodes returns a list of child and childnodes[1] returns the ul which is what we want
 document.getElementById('calendar_body').childNodes[1].addEventListener('click',
-function(e){
-  if(e.target && e.target.matches("li.date")){
-    console.log("date is clicked");
+  function(e) {
+    if (e.target && e.target.matches("li.date")) {
+      console.log("date is clicked");
+    }
+  });
+
+
+
+
+
+
+  // Get the modal
+  var modal = document.getElementById('myModal');
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
+      modal.style.display = "block";
   }
-});
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  } 
